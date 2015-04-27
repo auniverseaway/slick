e117650de4e9e107281c2c38e49cf8674838f822
Chris Millar <chris.millar@sandisk.com>
2015-04-27 17:20:42 -0600

    404 Support

---
+++ src/main/java/net/zum/slick/libs/NotFound.java
@@ -0,0 +1,16 @@
+package net.zum.slick.libs;
+
+import javax.script.Bindings;
+
+import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.scripting.SlingBindings;
+import org.apache.sling.scripting.sightly.pojo.Use;
+
+public class NotFound implements Use {
+
+    @Override
+    public void init(Bindings bindings) {
+        SlingHttpServletResponse response = (SlingHttpServletResponse) bindings.get(SlingBindings.RESPONSE);
+        response.setContentType("text/html");
+    }
+}
